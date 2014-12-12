6e72c214b49dedac2fac9383f3274c183f70fe28
Arnout Kazemier <info@3rd-Eden.com>
2014-12-12 22:34:38 +0100

    [minor] Add map method

--- array/index.js
+++ array/index.js
@@ -15,7 +15,23 @@ function clone(arr) {
   return result;
 }
 
+/**
+ * Map the values of the array to something else.
+ *
+ * @param {Array} arr Array who's results we should transform.
+ * @returns {Array} Mapped array.
+ * @api public
+ */
+function map(arr, fn) {
+  for (var i = 0, l = arr.length, result = new Array(l); i < l; i++) {
+    result[i] = fn(arr[i], i, arr);
+  }
+
+  return result;
+}
+
 //
 // Expose the methods.
 //
 exports.clone = clone;
+exports.map = map;
--- object/index.js
+++ object/index.js
@@ -20,7 +20,28 @@ function clone(obj) {
   return result;
 }
 
+/**
+ * Map the values of the object to something else.
+ *
+ * @param {Array} arr Array who's results we should transform.
+ * @returns {Array} Mapped array.
+ * @api public
+ */
+function map(obj, fn) {
+  var result = Object.create(null)
+    , keys = Object.keys(obj)
+    , length = keys.length
+    , i = 0;
+
+  for (; i < length; i++) {
+    result[keys[i]] = fn(obj[keys[i]], keys[i], obj);
+  }
+
+  return result;
+}
+
 //
 // Expose the methods.
 //
 exports.clone = clone;
+exports.map = map;
